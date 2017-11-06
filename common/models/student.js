26ce25be8f7bd4cf5bcd77858f5108e49bcb3fb9
norman404 <tonygtz51@gmail.com>
2017-11-06 01:26:46 -0600

    Crear metodos remotos

--- common/models/student.js
+++ common/models/student.js
@@ -1,5 +1,11 @@
 'use strict';
 
 module.exports = function(Student) {
-
+  Student.greet = function (msg, cb) {
+  	cb(null, 'Greetings... '+ msg);
+  }
+  Student.remoteMethod('greet',{
+  	accepts:{arg:"msn", type:'string'},
+  	returns:{arg:"greetings", type: 'string'}
+  });
 };
